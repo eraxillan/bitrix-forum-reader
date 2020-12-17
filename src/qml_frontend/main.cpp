002f984aec56e4ca3dd33085739b6970d8937648
Alexander Kamyshnikov <axill777@gmail.com>
2020-12-17 19:13:06 +0300

    Common: MIT license notice added to all source code files

--- src/qml_frontend/main.cpp
+++ src/qml_frontend/main.cpp
@@ -1,4 +1,27 @@
-﻿#include <QGuiApplication>
+﻿/*
+ * This file is part of Bitrix Forum Reader.
+ *
+ * Copyright (C) 2016-2020 Alexander Kamyshnikov <axill777@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QSettings>
@@ -26,7 +49,8 @@
 #include "website_backend/gumboparserimpl.h"
 #include "qml_frontend/forumreader.h"
 
-// FIXME: add copyright and license info in source files
+// FIXME: cleanup includes and prepend Qt ones with module path
+// FIXME: update QML imports to modern versions
 // FIXME: move raw strings (error messages etc.) to separate file and get them thru new LocalizationManager class
 //
 // FIXME: enforce constness
--- src/website_backend/websiteinterface_qt.h
+++ src/website_backend/websiteinterface_qt.h
@@ -1,5 +1,28 @@
-#ifndef WEBSITEINTERFACE_QT_H
-#define WEBSITEINTERFACE_QT_H
+/*
+ * This file is part of Bitrix Forum Reader.
+ *
+ * Copyright (C) 2016-2020 Alexander Kamyshnikov <axill777@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#ifndef __BFR_WEBSITEINTERFACE_QT_H__
+#define __BFR_WEBSITEINTERFACE_QT_H__
 
 #include <QtCore/QDateTime>
 
@@ -78,4 +101,4 @@ public:
 Q_DECLARE_METATYPE(PostQtWrapper)
 Q_DECLARE_METATYPE(UserQtWrapper)
 
-#endif // WEBSITEINTERFACE_QT_H
+#endif // __BFR_WEBSITEINTERFACE_QT_H__
